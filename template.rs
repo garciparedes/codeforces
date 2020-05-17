228c41045969ddbf8d879501f25c9ae35be1fd0b
Sergio García Prado <sergio@garciparedes.me>
2020-05-17 17:37:27 +0200

    "* Upgraded template file."

---
+++ template.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)?;
+    let n = input.trim().parse().unwrap();
+    for _ in 0..n {
+        input.clear();
+        io::stdin().read_line(&mut input)?;
+    
+        let result = solve(&input);
+
+        println!("{}", result);
+
+    }
+
+    return Ok(());
+}
+
+fn solve(input: &String) -> String {
+    let mut result = String::new();
+
+    result.push_str(input);
+
+    return result;
+}
